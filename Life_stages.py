0dae9face65b13e53aa0064720cfc166559c2502
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-14 01:09:09 +0800

    I forgot to put the kid part earlier

--- Life_stages.py
+++ Life_stages.py
@@ -12,8 +12,10 @@
 age = int(input("Age: "))
 
 # 2. Test if kid
-if age >= 13 and age <=17:
-    print("Teen")
+if age > -1 and age <= 12:
+    print("Kid")
 # 3. Test if teen
+elif age >= 13 and age <=17:
+    print("Teen")
 # 4. Test if debut
 # 5. sure adult
