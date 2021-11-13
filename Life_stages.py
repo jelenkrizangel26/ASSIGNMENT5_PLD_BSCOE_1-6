1dd87262b04409731597129a0aa2e183d5550704
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-14 01:03:10 +0800

    Done already on the teen part

---
+++ Life_stages.py
@@ -0,0 +1,19 @@
+# Program 3: Life stages
+# Create a program that ask for an age of a person
+# Display the life stage of the person.
+# Rules:
+# 0 - 12 : Kid
+# 13 - 17 : Teen
+# 18 : Debut
+# 19 above : Adult
+
+# steps:
+# 1. Ask for the age,convert and store
+age = int(input("Age: "))
+
+# 2. Test if kid
+if age >= 13 and age <=17:
+    print("Teen")
+# 3. Test if teen
+# 4. Test if debut
+# 5. sure adult
