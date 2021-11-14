d2d2c822d54a7e9414966cb3da282b54ebdef3ee
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-14 18:08:40 +0800

    Im feel like giving up on my idea but lets keep trying

--- Finding_lwst#.py
+++ Finding_lwst#.py
@@ -9,6 +9,9 @@ your_1stNO = float(input("Please enter your first number: "))
 your_2ndNO = float(input("Please enter your second number: "))
 your_3rdNO = float(input("Please enter your third number: "))
 
+name1 = float(input("Please put the name of 1st number owner: "))
+name2 = float(input("Please put the name of 2st number owner: "))
+name3 = float(input("Please put the name of 3rd number owner: "))
 
 def display_nmbr(Amnt1, Amnt2, Amnt3):
     if Amnt1 < Amnt2 and Amnt1 < Amnt3:
@@ -17,7 +20,12 @@ def display_nmbr(Amnt1, Amnt2, Amnt3):
         return Amnt2
     elif Amnt3 < Amnt1 and Amnt3 < Amnt2:
         return Amnt3
-    
-lowest_nmbr = display_nmbr(Amnt1 = your_1stNO, Amnt2 = your_2ndNO, Amnt3 = your_3rdNO)
+def display_name(name1, name2, name3):
+    name1 = your_1stNO
+    name2 = your_2ndNO
+    name3 = your_3rdNO
 
-print(f"The lowest input number in the your values is {lowest_nmbr}.")
+lowest_nmbr = display_nmbr(Amnt1 = your_1stNO, Amnt2 = your_2ndNO, Amnt3 = your_3rdNO)
+names = display_name(your_1stNO = name1, your_2ndNO = name2, your_3rdNO = name3)
+
+print(f"{names} lowest input number in the your values is {lowest_nmbr}.")
