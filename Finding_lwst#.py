b7b8541ea49f689fd1c8b3cf6ec9b0e485fe2d4f
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-14 16:55:19 +0800

    Done with my intro

---
+++ Finding_lwst#.py
@@ -0,0 +1,6 @@
+# Program 2: Find the lowest number
+# Create a program that ask 3 numbers. 
+# Find the lowest number using only if-else statement.
+# Display the lowest number
+
+print("Hi! I am Krizzy and I will help you to find the lowest number that you have. Just please follow the instructions. Thank you!")
