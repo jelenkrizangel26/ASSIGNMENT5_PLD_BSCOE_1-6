79cd9f828345ac172b8f711c2c43117b1a065b41
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-14 18:46:57 +0800

    I cant still get it, I will just eat then go for it again

--- Finding_lwst#.py
+++ Finding_lwst#.py
@@ -9,9 +9,9 @@ your_1stNO = float(input("Please enter your first number: "))
 your_2ndNO = float(input("Please enter your second number: "))
 your_3rdNO = float(input("Please enter your third number: "))
 
-name1 = float(input("Please put the name of 1st number owner: "))
-name2 = float(input("Please put the name of 2st number owner: "))
-name3 = float(input("Please put the name of 3rd number owner: "))
+name1 = input("Please put the name of 1st number owner: ")
+name2 = input("Please put the name of 2st number owner: ")
+name3 = input("Please put the name of 3rd number owner: ")
 
 def display_nmbr(Amnt1, Amnt2, Amnt3):
     if Amnt1 < Amnt2 and Amnt1 < Amnt3:
@@ -20,12 +20,15 @@ def display_nmbr(Amnt1, Amnt2, Amnt3):
         return Amnt2
     elif Amnt3 < Amnt1 and Amnt3 < Amnt2:
         return Amnt3
-def display_name(name1, name2, name3):
-    name1 = your_1stNO
-    name2 = your_2ndNO
-    name3 = your_3rdNO
+
+def display_names():
+    naMe1 = name1
+    naMe2 = name2
+    naMe3 = name3
+    return naMe1, naMe2, naMe3
 
 lowest_nmbr = display_nmbr(Amnt1 = your_1stNO, Amnt2 = your_2ndNO, Amnt3 = your_3rdNO)
-names = display_name(your_1stNO = name1, your_2ndNO = name2, your_3rdNO = name3)
+names = display_names(naMe1 = name1 , name2 = name2, naMe3 = name3)
+owner = lowest_nmbr = names
 
-print(f"{names} lowest input number in the your values is {lowest_nmbr}.")
+print(f"{owner} lowest input number in the your values is {lowest_nmbr}.")
