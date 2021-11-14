1f1295afc4fa86b13614c109523c1eacece6e453
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-14 17:10:03 +0800

    So I encounter some error on this part since I forgot to double the parenthesis

--- Finding_lwst#.py
+++ Finding_lwst#.py
@@ -3,4 +3,17 @@
 # Find the lowest number using only if-else statement.
 # Display the lowest number
 
-print("Hi! I am Krizzy and I will help you to find the lowest number that you have. Just please follow the instructions. Thank you!")
+print("Hi! I am Krizzy and I will help you to find the lowest number that you have. Just please give the things that will ask. Thank you")
+
+your_1stNO = float(input("Please enter your first number: "))
+your_2ndNO = float(input("Please enter your second number: "))
+your_3rdNO = float(input("Please enter your third number: "))
+
+def display_nmbr(Amnt1, Amnt2, Amnt3):
+    if Amnt1 < Amnt2 and Amnt1 < Amnt3:
+        return Amnt1
+    elif Amnt2 < Amnt1 and Amnt2 < Amnt3:
+        return Amnt2
+    elif Amnt3 < Amnt1 and Amnt3 < Amnt2:
+        return Amnt3
+    
