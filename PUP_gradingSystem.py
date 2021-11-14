c9f8c08968b335772fda83f04020a5566646195e
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-14 11:29:18 +0800

    Yeyy! Done already on grading system, one program to go!

--- PUP_gradingSystem.py
+++ PUP_gradingSystem.py
@@ -6,9 +6,8 @@
 # Grade/Mark: 1.75
 # Description: Very Good
 
-import math 
 grd_ = float(input("Grade: "))
-grd = math.ceil (grd_)
+grd = round(grd_)
 
 if grd >= 97 and grd <= 100:
     print("Excellent! Your grade is 1.0!")
@@ -16,3 +15,20 @@ elif grd >= 94 and grd <= 96:
     print("Excellent! Your grade is 1.25!")
 elif grd >= 91 and grd <= 93:
     print("Very Good! Your grade is 1.5!")
+
+elif grd >= 88 and grd <= 90:
+    print("Very Good! Your grade is 1.75!")
+elif grd >= 85 and grd <= 87:
+    print("Good! Your grade is 2.0!")
+elif grd >= 82 and grd <= 84:
+    print("Good! Your grade is 2.25!")
+elif grd >= 79 and grd <= 81:
+    print("Satisfactory! Your grade is 2.50")
+elif grd >= 76 and grd <= 78:
+    print("Satisfactory! Your grade is 2.75!")
+elif grd >= 75:
+    print("Passing! Your grade is 3.0")
+elif grd >= 65 and grd <= 74:
+    print("Failure! Your grade is 5.0 :)")
+else:
+    print("Your are either Incomplete (Inc), Withdrawn (W), or Dropped (D) ")
